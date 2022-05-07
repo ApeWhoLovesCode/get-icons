13a030ee911c8ffdf2272d39d69834dc58b4c1f9
木亘 <970519495@qq.com>
2022-05-07 15:44:35 +0800

    修改配置

--- src/svg-data/asn/PlayLinear.ts
+++
@@ -1,7 +0,0 @@
-// This icon file is generated automatically.
-
-import { IconDefinition } from '../types';
-
-const PlayLinear: IconDefinition = {"icon":{"tag":"svg","attrs":{"overflow":"hidden","style":"width:1em;height:1em;vertical-align:middle","viewBox":"64 64 896 896","focusable":"false"},"children":[{"tag":"path","attrs":{"d":"M512 37.89C251.24 37.89 37.89 251.29 37.89 512S251.24 986.11 512 986.11 986.11 772.76 986.11 512 772.71 37.89 512 37.89zm0 908.7C273 946.59 77.41 751 77.41 512 77.41 273 273 77.41 512 77.41 751 77.41 946.59 273 946.59 512 946.59 751 751 946.59 512 946.59z"}},{"tag":"path","attrs":{"d":"M660.74 444.42L499.2 338.94c-54.53-35.84-100.97-11.93-100.97 53.71v238.6c0 65.63 44.4 89.5 100.97 53.7l161.54-105.37c54.52-39.78 54.52-99.43 0-135.27z"}}]},"name":"play","theme":"linear"};
-
-export default PlayLinear;
